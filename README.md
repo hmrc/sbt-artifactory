f1d7c6a9098bd486ce050a0728ae838c69088bf1
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-05-21 10:34:10 +0100

    PBD-3195 Update to only publish to local directory and remove unpublish

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 
  [ ![Download](https://img.shields.io/github/v/release/hmrc/sbt-artifactory) ](https://open.artefacts.tax.service.gov.uk/ivy2/uk.gov.hmrc/sbt-artifactory/scala_2.12/sbt_1.0/)
 
+SbtArtifactory is no longer needed in projects, it is only for use on the build servers.
+
+### 2.x.x
+
+The plugin only configures `publishTo` to write artefacts to a local directory. The actual remote publishing and unpublishing will be handled by build-jobs.
+
 ### Sbt 1.x
 
 Since major version 1, this plugin is cross compiled for sbt 1.x (specifically 1.3.4).
--- src/main/scala/uk/gov/hmrc/SbtArtifactory.scala
+++ src/main/scala/uk/gov/hmrc/SbtArtifactory.scala
@@ -16,115 +16,23 @@
 
 package uk.gov.hmrc
 
-import org.scalajs.sbtplugin.ScalaJSCrossVersion
-import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport.isScalaJSProject
-import sbt.Classpaths.publishTask
 import sbt.Keys._
 import sbt._
-import uk.gov.hmrc.sbtsettingkeys.Keys.isPublicArtefact
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
+object SbtArtifactory extends sbt.AutoPlugin {
 
-object SbtArtifactory extends sbt.AutoPlugin{
-
-  private val distributionTimeout = 1.minute
-
-  private val currentVersion = getClass.getPackage.getImplementationVersion
-
-  private lazy val maybeArtifactoryUri        = sys.env.get("ARTIFACTORY_URI")
-  private lazy val maybeArtifactoryUsername   = sys.env.get("ARTIFACTORY_USERNAME")
-  private lazy val maybeArtifactoryPassword   = sys.env.get("ARTIFACTORY_PASSWORD")
-
-  object autoImport {
-    val repoKey             = settingKey[String]("Artifactory repository name")
-    val artifactDescription = settingKey[ArtifactDescription]("Artifact description")
-    val unpublish           = taskKey[Unit]("Unpublish from Artifactory")
-  }
-
-  import autoImport._
+  object autoImport { } // required for AutoPlugins, even if empty
 
   private object EnvKeys {
     val publishPath = "PUBLISH_PATH"
   }
 
   override val projectSettings: Seq[Def.Setting[_]] = Seq(
-    publishMavenStyle   := !sbtPlugin.value,
-    licenses += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"),
-    artifactDescription := ArtifactDescription.withCrossScalaVersion(
-      org            = organization.value,
-      name           = name.value,
-      version        = version.value,
-      scalaVersion   = scalaVersion.value,
-      // sbtVersion needs to be resolved in the context of the pluginCrossBuild so it resolves correctly for cross-compiling sbt
-      sbtVersion     = (sbtVersion in pluginCrossBuild).value,
-      publicArtifact = isPublicArtefact.value,
-      sbtPlugin      = sbtPlugin.value,
-      scalaJsVersion = if (isScalaJSProject.value) Some(ScalaJSCrossVersion.currentBinaryVersion) else None
-    ),
-    repoKey := artifactoryRepoKey(sbtPlugin.value, isPublicArtefact.value),
     publishMavenStyle := !sbtPlugin.value,
     publishTo :=
       sys.env.get(EnvKeys.publishPath)
         .map(p => Resolver.file("file", file(p))(if (sbtPlugin.value) Resolver.ivyStylePatterns else Resolver.mavenStylePatterns))
-        .orElse {
-          maybeArtifactoryUri.map { uri =>
-            if (sbtPlugin.value)
-              Resolver.url(repoKey.value, url(s"$uri/${repoKey.value}"))(Resolver.ivyStylePatterns)
-            else
-              "Artifactory Realm" at s"$uri/${repoKey.value}"
-          }
-        },
-    credentials ++= {
-      // DirectCredentials.toString is not implemented for sbt 0.13
-      val credString = maybeArtifactoryCredentials.map(cred => s"DirectCredentials(${cred.realm}, ${cred.host}, ${cred.userName}, ****)")
-      sLog.value.info(message(name.value, s"Configuring Artifactory credentials: $credString"))
-      maybeArtifactoryCredentials.toSeq
-    },
-    unpublish := {
-      sLog.value.info(message(name.value, "Unpublishing from Artifactory..."))
-      Await.result(
-        artifactoryConnector(repoKey.value)
-          .deleteVersion(artifactDescription.value, sLog.value),
-        distributionTimeout
-      )
-    },
-    publish := {
-      sLog.value.info(message(name.value, "Publishing to Artifactory..."))
-      // this deprecated function is required for sbt 0.13
-      publishTask(publishConfiguration, deliver).value
-    }
   )
 
   override def trigger = allRequirements
-
-  private[hmrc] def artifactoryRepoKey(isSbtPlugin: Boolean, publicArtifact: Boolean): String =
-    (isSbtPlugin, publicArtifact) match {
-      case (false, false) => "hmrc-releases-local"
-      case (false, true)  => "hmrc-public-releases-local"
-      case (true, false)  => "hmrc-sbt-plugin-releases-local"
-      case (true, true)   => "hmrc-public-sbt-plugin-releases-local"
-    }
-
-  private def maybeArtifactoryCredentials: Option[DirectCredentials] =
-    for {
-      uri      <- maybeArtifactoryUri
-      username <- maybeArtifactoryUsername
-      password <- maybeArtifactoryPassword
-    } yield Credentials(
-        realm    = "Artifactory Realm",
-        host     = new URI(uri).getHost,
-        userName = username,
-        passwd   = password
-      ).asInstanceOf[DirectCredentials]
-
-  private def artifactoryConnector(repositoryName: String): ArtifactoryConnector =
-    new ArtifactoryConnector(
-      httpClient     = DispatchCrossSupport.http,
-      credentials    = maybeArtifactoryCredentials.getOrElse(sys.error(s"SbtArtifactoryPlugin - No credential was found found")),
-      repositoryName = repositoryName
-    )
-
-  private def message(projectName: String, msg: String): String =
-    s"SbtArtifactoryPlugin [$currentVersion] ($projectName) - $msg"
 }
